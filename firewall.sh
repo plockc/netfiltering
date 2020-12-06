341af23dee80deccffa22e308baf40c7d5596a30
Chris Plock <>
2020-12-05 21:25:54 -0800

    latest

--- firewall.sh
+++ firewall.sh
@@ -1,3 +1,49 @@
 #!/bin/bash
 set -euo pipefail
-sudo apt-get -y install iptables-persistent
+apt-get -y install iptables-persistent
+iptables -F # Flush filter table
+iptables -X # Delete all filter chains
+iptables -Z # Flush all counters too
+
+iptables -t nat -F
+iptables -t nat -X
+
+iptables -t mangle -F
+iptables -t mangle -X
+
+iptables -t raw -F
+iptables -t raw -X
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
+iptables -A INPUT -p icmp -m icmp --icmp-type 0 -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
+iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
+iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
+iptables -t nat -P PREROUTING ACCEPT
+iptables -t nat -P INPUT ACCEPT
+iptables -t nat -P OUTPUT ACCEPT
+iptables -t nat -P POSTROUTING ACCEPT
+
+iptables -t raw -P PREROUTING ACCEPT
+iptables -t raw -P OUTPUT ACCEPT
+
+iptables -t security -P INPUT ACCEPT
+iptables -t security -P OUTPUT ACCEPT
+iptables -t security -P FORWARD ACCEPT
+
+iptables -t mangle -P PREROUTING ACCEPT
+iptables -t mangle -P INPUT ACCEPT
+iptables -t mangle -P FORWARD ACCEPT
+iptables -t mangle -P OUTPUT ACCEPT
+iptables -t mangle -P POSTROUTING ACCEPT
+ip6tables -A INPUT -j REJECT --reject-with adm-prohibited
+ip6tables -A OUTPUT -j REJECT --reject-with adm-prohibited
+ip6tables -A FORWARD -j REJECT --reject-with adm-prohibited
